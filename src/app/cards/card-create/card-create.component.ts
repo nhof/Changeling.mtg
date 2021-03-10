14d4064b7364d67bf7a81225e0f79f62c06ee5c1
nhof <ugh@gmx.li>
2021-03-10 22:30:06 +0100

    All Working

---
+++ src/app/cards/card-create/card-create.component.ts
@@ -0,0 +1,14 @@
+import {Component } from '@angular/core'
+
+@Component({
+selector: 'app-card-create',
+templateUrl: './card-create.component.html'
+})
+
+export class CardCreateComponent {
+  enteredRulestext = ""
+  newRulestext = "NoContent"
+  onCardSave(){
+  this.newRulestext = this.enteredRulestext;
+  }
+}
