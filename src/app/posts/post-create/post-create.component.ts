2806a60db2bec5ff64758362ab62c507d865ec8d
nhof <ugh@gmx.li>
2021-03-16 22:12:58 +0100

    Editing finished

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -21,7 +21,9 @@ constructor(public postsService: PostsService, public route:ActivatedRoute){}
       if(paramMap.has('postId')){
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
-        this.post = this.postsService.getPost(this.postId);
+        this.postsService.getPost(this.postId).subscribe(postData =>{
+          this.post = {id: postData._id, title: postData.title, text: postData.text}
+        });
       } else{
         this.mode = 'create';
         this.postId = null;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -31,7 +31,7 @@ export class PostsService
   }
 
   getPost(id: string){
-    return {...this.posts.find(p=> p.id === id)}
+    return this.http.get<{_id: string; title: string; text: string}>('http://localhost:3000/api/posts/'+id)
   }
 
   getPostsUpdeteListener(){
@@ -58,7 +58,13 @@ export class PostsService
       id: id, title: title, text: text
     };
     this.http.put('http://localhost:3000/api/posts/'+id, post)
-    .subscribe(response=> console.log(response));
+    .subscribe(response=>{
+      const updatedPosts = [...this.posts];
+      const oldPostIndex = updatedPosts.findIndex(p =>p.id === post.id) ;
+      updatedPosts [oldPostIndex] = post;
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts])
+    } );
   }
 
   deletePost(postId: string){
