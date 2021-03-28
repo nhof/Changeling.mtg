7a749a37be361b7e2944b8a307e67adb6cbe5c1d
nhof <ugh@gmx.li>
2021-03-28 20:26:17 +0200

    Login small changes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 
 @Component({
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
 
 export class LoginComponent {
   isLoading = false;
+
+  onLogin(form: NgForm){
+    console.log(form.value);
+  }
 }
 
