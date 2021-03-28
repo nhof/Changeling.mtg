a9c7d85a1560218d81216ffcae194c3537730217
nhof <ugh@gmx.li>
2021-03-28 12:56:02 +0200

    Auth html

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+ templateUrl: './login.component.html',
+ styleUrls: ['./login.component.scss']
+})
+
+export class LoginComponent {
+  isLoading = false;
+}
+
