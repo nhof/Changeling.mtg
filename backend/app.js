0b83f608cd56a68c91a2551894cc91ce36016b52
nhof <ugh@gmx.li>
2021-03-28 21:40:56 +0200

    Authentification backend

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const mongoose = require('mongoose');
 
 const postsRoutes = require('./routes/posts')
+const userRoutes = require('./routes/user')
 
 
 const app = express();
@@ -32,5 +33,6 @@ app.use((req,res,next)=>{
 });
 
 app.use("/api/posts",postsRoutes);
+app.use("/api/user",userRoutes);
 
 module.exports = app;
