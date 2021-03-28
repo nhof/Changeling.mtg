73f3c6cc5bf23736e84b864eec0ad89970ece306
nhof <ugh@gmx.li>
2021-03-28 22:13:15 +0200

    Authentification jwt

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -24,5 +25,33 @@ router.post("/signup", (req,res,next)=>{
     });
   });
 });
+
+router.post("/login",(req,res,next)=>{
+  User.findOne({
+    email: req.body.email
+  }).then(user=>{
+    if(!user){
+      return res.status(401).json({
+        message: "Authentification failed. This User was not found!"
+      })
+    }
+    return bcrypt.compare(req.body.password, user.password).then(result=>{
+      if(!result){
+        return res.status(401).json({
+          message: "Authentification failed."
+        })
+      }
+      const token = jwt.sign(
+        {email: user.email, userId: user._id},
+        'secret_this_should_be_longer',
+        {expiresIn: '1h'}
+        )
+    }).catch(err=>{
+      return res.status(401).json({
+        message: "Authentification failed."
+      })
+    })
+  });
+})
 
 module.exports = router;
