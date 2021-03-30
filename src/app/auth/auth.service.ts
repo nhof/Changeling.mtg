5df7a10ca797e7974994fa8617182d22444c3dd3
nhof <ugh@gmx.li>
2021-03-30 20:37:16 +0200

    Authentification Token expire

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,6 +11,7 @@ export class AuthService{
   private isAuthenticated = false;
   private token: string;
   private authStatusListener = new Subject<boolean>();
+  private tokenTimer: any;
 
   constructor(private http: HttpClient, private router: Router){}
 
@@ -36,11 +37,15 @@ export class AuthService{
 
   login(email: string, password: string){
     const authData: AuthData = {email: email, password: password}
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string, expiresIn: number}>("http://localhost:3000/api/user/login", authData)
     .subscribe(response=>{
       const token = response.token;
       this.token = token;
       if(token){
+        const expiresInDuration = response.expiresIn;
+        this.tokenTimer = setTimeout(()=>{
+          this.logout();
+        }, expiresInDuration*1000);
         this.isAuthenticated = true;
         this.authStatusListener.next(true);
         this.router.navigate(['/'])
@@ -52,7 +57,8 @@ export class AuthService{
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
-    this.router.navigate(['/'])
+    clearTimeout(this.tokenTimer);
+    this.router.navigate(['/']);
   }
 
 }
