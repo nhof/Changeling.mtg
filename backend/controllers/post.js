a02538bb7491b9b9a93dd250137ae9c065e5ffe1
nhof <ugh@gmx.li>
2021-03-31 17:16:49 +0200

    Authorization Module

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -76,28 +76,27 @@ exports.deletePost = (req,res, next)=>{
 });;
 }
 
-exports.updatePost = (req, res, next)=>
-{
+exports.updatePost = (req, res, next)=>{
   let imagePath = req.body.imagePath;
   if(req.file){
     const url = req.protocol + '://' + req.get("host");
     imagePath = url + "/images/" + req.file.filename
   }
   const post = new Post({
-  _id: req.body.id,
-  title: req.body.title,
-  text: req.body.text,
-  imagePath: imagePath,
-  creator: req.userData.userId
-})
-Post.updateOne({_id: req.params.id, creator: req.userData.userId}, post).then(result =>{
-  if(result.n>0){
-    res.status(200).json({message: "Update successful!"});
-  } else {
-    console.log("Problems")
-    res.status(401).json({message: "Not authorized!"});
-  }
-}).catch(error=>{
-  res.status(500).json({message:"Couldn't update post"})
-})
+    _id: req.body.id,
+    title: req.body.title,
+    text: req.body.text,
+    imagePath: imagePath,
+    creator: req.userData.userId
+  })
+  Post.updateOne({_id: req.params.id, creator: req.userData.userId}, post).then(result =>{
+    if(result.n>0){
+      res.status(200).json({message: "Update successful!"});
+    } else {
+      console.log("Problems")
+      res.status(401).json({message: "Not authorized!"});
+    }
+  }).catch(error=>{
+    res.status(500).json({message:"Couldn't update post"})
+  })
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AngularMaterialModule} from './angular-material.module'
 
@@ -9,29 +8,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { ErrorComponent } from './error/error.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import  { ErrorInterceptor} from './error-interceptor'
 import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent
   ],
   imports: [
-    FormsModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     AngularMaterialModule,
-    PostsModule
+    PostsModule,
+    AuthModule
     ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
