3f63fa59af584f63ad910f03262a59ee8db0c1e6
nhof <ugh@gmx.li>
2021-04-02 04:43:58 +0200

    Home Added images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit} from "@angular/core";
+
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,29 @@ import { Component } from "@angular/core";
   styleUrls: ['./home.component.scss']
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
   constructor(){}
+
+  imgSrc = "http://localhost:3000/images/default/default"
+
+  ngOnInit(){
+    const rnd = Math.floor(Math.random()*4)
+    console.log(rnd)
+    switch(rnd){
+      case 0:
+        this.imgSrc +="_01.jpg";
+      break;
+    case 1:
+      this.imgSrc +="_02.jpg";
+      break;
+    case 2:
+      this.imgSrc +="_03.jpg";
+      break;
+    case 3:
+      this.imgSrc +="_04.jpg";
+      break;
+    }
+  };
 
 }
